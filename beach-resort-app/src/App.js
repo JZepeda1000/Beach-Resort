e35488f7bd16e82e3ecfc77965557026bf50d71a
JZepeda1000 <jz1043@yahoo.com>
2020-05-24 09:58:04 -0500

    Created navbar

--- beach-resort-app/src/App.js
+++ beach-resort-app/src/App.js
@@ -5,15 +5,19 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms/" component={Rooms} />
-      <Route exact path="/rooms/:description" component={SingleRoom} />
-      <Route component={Error} />
-    </Switch>
+    <div>
+    <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:description" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </div>
   )
 };
 
