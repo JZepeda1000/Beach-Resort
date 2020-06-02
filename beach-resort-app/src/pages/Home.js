eb9f67a559300d94c2be11ea01dab5075abbe2f4
JZepeda1000 <jz1043@yahoo.com>
2020-06-02 09:31:00 -0500

    Working on FeaturedRooms component

--- beach-resort-app/src/pages/Home.js
+++ beach-resort-app/src/pages/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRooms';
 import { Link } from 'react-router-dom';
 
-const Home = () => {
+export default function Home() {
     return (
-        <div>
+        <>
             <Hero>
                 <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
                     <Link to="/rooms" className="btn-primary">
@@ -15,8 +16,7 @@ const Home = () => {
                 </Banner>
             </Hero>
             <Services />
-        </div>
+        </>
     )
 };
 
-export default Home;
