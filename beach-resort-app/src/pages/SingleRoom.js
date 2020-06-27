4d5fe0beac309cfddd8fd906ff51ae37b6f1d2a7
JZepeda1000 <jz1043@yahoo.com>
2020-06-27 10:55:18 -0500

    Worked on SingleRoom component

--- beach-resort-app/src/pages/SingleRoom.js
+++ beach-resort-app/src/pages/SingleRoom.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../context';
 
 export default class SingleRoom extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            slug:this.props.match.params.slug,
+            defaultBcg
+        };
+    }
+
+    static contextType = RoomContext;
+
+    componentDidMount() {
+
+    }
+ 
     render() {
-        return (
-            <div>
-                Hello from Single Room Page
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        if(!room) {
+            return <div className="error">
+                <h3>No Such Room Could Be Found...</h3>
+                <Link to='/rooms' className="btn-primary">
+                    Back To Rooms
+                </Link>
             </div>
-        )
+        }
+
+        const {
+            name,
+            description,
+            capacity,
+            size,
+            price,
+            extras,
+            breakfast,
+            pets,
+            images
+        } = room
+
+        return <Hero hero='roomsHero'>
+            <Banner title={`${name} room`} />
+            <Link to='/rooms' className='btn-primary'>
+                Back TO Rooms
+            </Link>
+        </Hero>
     }
 };
